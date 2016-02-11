3d15e62e4e824b039606289b3ffa396174ef2371
israa92 <israa.alazam@yahoo.se>
2016-02-11 12:32:26 +0100

    init commit

--- DoctorWPF/Registration.xaml.cs
+++ DoctorWPF/Registration.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,11 +19,64 @@ namespace DoctorWPF
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class Registration : Window
     {
-        public Login()
+        public Registration()
         {
             InitializeComponent();
+        }
+
+        private void submit_click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email!";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch
+                (textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string username = textBoxUsername.Text;
+                string email = textBoxEmail.Text;
+                int age = Convert.ToInt32(textBoxAge.Text);
+                string password = passwordBox1.Password;
+                if (passwordBox1.Password.Length == 0)
+                {
+                    errormessage.Text = "Enter password.";
+                    passwordBox1.Focus();
+                }
+                else if (passwordBoxConfirm.Password.Length == 0)
+                {
+                    errormessage.Text = "Enter confirm password";
+                    passwordBoxConfirm.Focus();
+                }
+                else if (passwordBox1.Password != passwordBoxConfirm.Password)
+                {
+                    errormessage.Text = "Confirm password must be same as password";
+                    passwordBoxConfirm.Focus();
+                }
+                else
+                {
+                    errormessage.Text = "";
+                    SqlConnection con = new SqlConnection("Data Source=(localdb)\mssqllocaldb;Initial Catalog=Doctor;Integrated Security=True;Pooling=False");
+                }
+            }
+        }
+
+        private void cancel_click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
